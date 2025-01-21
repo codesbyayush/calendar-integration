23171670853da11549d84f78ca05fbf0a9cf1a8f
Ayush Patel <codesbyayush@gmail.com>
2025-01-21 06:46:56 +0530

    add a note

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import { SignIn } from '@/components/sign-in';
 import { redirect } from 'next/navigation';
 import { auth } from '@/auth';
 
+import { PencilRuler } from 'lucide-react';
+
 export default async function Home() {
 
   const session = await auth();
@@ -17,6 +19,11 @@ export default async function Home() {
       {session ? (
         <div>
           <Calendar />
+          <p className='max-w-screen-xl mx-auto text-center w-full text-sm flex gap-3 items-center'>
+            <PencilRuler className='size-4' /> <span className='font-medium'>
+              To improve the display of recurring events, they are combined on this page. This can sometimes result in fewer events being shown, causing the "Next" button to appear inactive.
+            </span>
+          </p>
         </div>
 
       ) : <SignIn />}
